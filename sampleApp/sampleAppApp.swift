5f55394a98f5882ac7ccac8c9b411edb4ae79b72
freddie <chakangost@gmail.com>
2021-09-29 09:16:50 +0900

    Initial Commit

---
+++ sampleApp/sampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleAppApp.swift
+//  sampleApp
+//
+//  Created by freddie on 2021/09/29.
+//
+
+import SwiftUI
+
+@main
+struct sampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
